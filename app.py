77cae4dc3e8763a5d8c635fc9137b257b202ad83
Khushi Ganatra <khush@MacBook-Air-2.local>
2026-06-28 18:21:31 +0100

    Database creation

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
-from flask import Flask 
+from flask import Flask
+import sqlite3
+
 app = Flask(__name__)
+
+
+def init_db():
+    connection = sqlite3.connect("issues.db")
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS issues(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            description TEXT NOT NULL,
+            severity TEXT NOT NULL,
+            status TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
+    connection.commit()
+    connection.close()
+
 @app.route("/")
-<<<<<<< HEAD
-
 def home():
-  return "Welcome to the X-Bix TechVentures Issue & Vulnerability Tracking API"
-
-if __name__ == "__main__":
-    app.run(debug=True)
-=======
->>>>>>> 81858a6 (Updated Flask app and dependencies; added gitignore for venv)
-
-def home():
-
     return "Welcome to the X-Bix TechVentures Issue & Vulnerability Tracking API"
 
 if __name__ == "__main__":
