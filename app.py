b309287733edf64e5d9186bf7c6c7687167fa3ea
Khushi Ganatra <khush@MacBook-Air-2.local>
2026-06-28 12:49:20 +0100

    Updated Flask app and dependencies; added gitignore for venv

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
 from flask import Flask 
 app = Flask(__name__)
 @app.route("/")
+<<<<<<< HEAD
 
 def home():
   return "Welcome to the X-Bix TechVentures Issue & Vulnerability Tracking API"
 
 if __name__ == "__main__":
     app.run(debug=True)
+=======
+>>>>>>> 81858a6 (Updated Flask app and dependencies; added gitignore for venv)
 
+def home():
+
+    return "Welcome to the X-Bix TechVentures Issue & Vulnerability Tracking API"
+
+if __name__ == "__main__":
+    app.run(debug=True)
