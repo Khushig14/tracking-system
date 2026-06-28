1f10d0597b827668c4b99597f7da11f6172e2020
Khushi <khushiganatra14@gmail.com>
2026-06-28 12:20:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
+from flask import Flask 
+app = Flask(__name__)
+@app.route("/")
+
+def home():
+  return "Welcome to the X-Bix TechVentures Issue & Vulnerability Tracking API"
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
